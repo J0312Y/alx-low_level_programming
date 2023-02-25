ee1ecacec6ceff4d2531071a291070c0e9289aab
J0312Y <techevolu77@gmail.com>
2023-02-25 07:40:48 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * joeldy times_table - prints the 9 times table, starting with 0
+ * times_table - prints the 9 times table, starting with 0
  */
 void times_table(void)
 {
